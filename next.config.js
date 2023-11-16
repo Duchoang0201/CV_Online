cef649ca7656587ed23d27a0c9e6d56b60517425
HOANGNBD <Duchoang020120@gmail.com>
2023-11-16 13:58:24 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./my-loader.ts",
+  },
 };
+
 module.exports = nextConfig;
